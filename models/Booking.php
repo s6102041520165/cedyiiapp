ee568b3ac5236c6425c4af5f4f58a970fc784ca0
s6102041520165 <s6102041520165@email.kmutnb.ac.th>
2019-03-08 21:42:27 +0700

    Enter

---
+++ models/Booking.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class Booking extends Model
+{
+    public $fname;
+    public $lname;
+    public $tel;
+}
---
+++ views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+use yii\base\View;
+/* @var $this yii\web\View */
+$this->title='ComEdu Homecoming';
+?>
+
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            
+        </div>
+
+    </div>
+</div>
